9a40b64d3073f32903730f0c8edc3a4a87163b94
Soufiane Majd <majdsoufiane55@gmail.com>
2026-05-22 11:25:05 +0100

    Exclude secrets.h from repository for security

--- sketch_may2a/secrets.h
+++
@@ -1,5 +0,0 @@
-// Ce fichier ne doit pas être partagé publiquement ou mis sur GitHub
-// Il contient tes informations de connexion sensibles
-
-#define SECRET_SSID "Realme X"
-#define SECRET_PASS "87654321..."
